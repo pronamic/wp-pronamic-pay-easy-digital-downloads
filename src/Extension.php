9fd43b71d3d9e6f7acb3297fea925aacc2690c53
Remco Tolsma <info@remcotolsma.nl>
2016-02-04 09:43:05 +0100

    Getting ready for version 1.2.2.

--- src/Extension.php
+++ src/Extension.php
@@ -6,7 +6,7 @@
  * Copyright: Copyright (c) 2005 - 2015
  * Company: Pronamic
  * @author Remco Tolsma
- * @version 1.2.0
+ * @version 1.2.2
  * @since 1.0.0
  */
 class Pronamic_WP_Pay_Extensions_EDD_Extension {
