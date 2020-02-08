aff27f98231bef25b8000eb63233cce73f34e098
Reüel van der Steege <reuel@pronamic.nl>
2020-02-08 11:38:06 +0100

    Extend `Extension` class from `AbstractPluginIntegration`.

--- src/Extension.php
+++ src/Extension.php
@@ -18,7 +18,7 @@ use Pronamic\WordPress\Pay\Plugin;
  * @version 2.0.6
  * @since   1.0.0
  */
-class Extension {
+class Extension extends \Pronamic\WordPress\Pay\AbstractPluginIntegration {
 	/**
 	 * Bootstrap
 	 */
