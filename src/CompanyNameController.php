cd51a1255a0822e9ada25982a9530550e423ecb8
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2022-03-04 11:19:35 +0100

    Add name attribute for company name field.

--- src/CompanyNameController.php
+++ src/CompanyNameController.php
@@ -180,7 +180,7 @@ class CompanyNameController {
 
 				<span class="edd-description"><?php \esc_html_e( 'Enter the name of your company.', 'pronamic_ideal' ); ?></span>
 
-				<input type="text" class="form-control edd-input" id="pronamic-edd-company-name" />
+				<input type="text" name="edd_company" class="form-control edd-input" id="pronamic-edd-company-name" />
 			</div>
 		</fieldset>
 		<?php
