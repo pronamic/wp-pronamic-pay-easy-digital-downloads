ec275b35fd8ccf04f391ea125060a699195eeb2d
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2026-06-24 11:06:52 +0200

    Add ABSPATH guard and autoload bootstrap
    Prevent direct access by exiting when ABSPATH is not defined, add an autoload require for vendor/autoload_packages.php, and include a bootstrap comment. This initializes package autoloading for the Easy Digital Downloads integration before registering the integration via the pronamic_pay_plugin_integrations filter.

--- pronamic-pay-easy-digital-downloads.php
+++ pronamic-pay-easy-digital-downloads.php
@@ -27,6 +27,18 @@
  * @package   Pronamic\WordPress\Pay\Extensions\EasyDigitalDownloads
  */
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Autoload.
+ */
+require_once __DIR__ . '/vendor/autoload_packages.php';
+
+/**
+ * Bootstrap.
+ */
 add_filter(
 	'pronamic_pay_plugin_integrations',
 	function ( $integrations ) {
